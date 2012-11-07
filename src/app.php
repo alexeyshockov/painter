795827cd75dc302828721dd5389e31fc3779728e
Alexey Shockov <alexey@shockov.com>
2012-11-07 08:23:52 +0000

    WIP on main logic

--- src/app.php
+++ src/app.php
@@ -4,6 +4,9 @@ use Silex\Application;
 
 $app = new Application();
 
+$app['buzz.browser'] = new Buzz\Browser();
+$app['imagine']      = new Imagine\Gd\Imagine();
+
 require __DIR__.'/controllers.php';
 
 return $app;
--- src/controllers.php
+++ src/controllers.php
@@ -3,3 +3,33 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\StreamedResponse;
+
+// TODO Memory limit check?..
+$app->get('.+', function(Request $request) use ($app) {
+    $scheme = $request->headers->get('X-Painter-Scheme', 'http');
+    $host   = $request->headers->get('X-Painter-Host', '127.0.0.1');
+    $uri    = $request->getRequestUri();
+
+    $proxyResponse = $app['buzz.browser']->get($scheme.'://'.$host.$uri);
+
+    // TODO Handle 404 and other errors.
+    // TODO Check content type...
+    $image = $proxyResponse->getContent();
+
+    // And transform...
+    $box    = $request->headers->get('X-Painter-Box', '100x100');
+    // TODO crop_face filter.
+    $filter = $request->headers->get('X-Painter-Filter', 'thumbnail');
+
+    list($width, $height) = explode('x', $box);
+
+    $size = new Imagine\Image\Box($width, $height);
+
+    $processedImage = $app['imagine']->load($image)->thumbnail($size);
+
+    return new StreamedResponse(
+        function() use($processedImage) {
+            $processedImage->show('jpeg');
+        }
+    );
+});
